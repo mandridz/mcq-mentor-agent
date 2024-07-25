192ae710552d3058b84c48cdfed840b9ab350443
Evgeny <evgeny.sidorenko@gmail.com>
2024-07-25 17:59:57 +0300

    localhost

--- perpl.py
+++ perpl.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from typing import Any, Dict, List
-from openai import OpenAI
+from lyzr_automata.ai_models.perplexity import PerplexityModel
 from lyzr_automata.ai_models.openai import OpenAIModel
 from lyzr_automata import Agent, Task
 from lyzr_automata.pipelines.linear_sync_pipeline import LinearSyncPipeline
@@ -12,20 +12,6 @@ load_dotenv()
 proxy_url = os.getenv("OPENAI_PROXY_URL")
 api_key = os.getenv("OPENAI_API_KEY")
 
-
-class CustomOpenAIModel(OpenAIModel):
-    def __init__(self, key, proxy, parameters: Dict[str, Any]):
-        self.parameters = parameters
-        # self.client = OpenAI(api_key=key) if proxy is None or proxy == "" else OpenAI(api_key=key,
-        #                                                                              http_client=httpx.Client(
-        #                                                                                  proxy=proxy))
-
-        print("key", key)
-        print("proxy", proxy)
-        self.client = OpenAI(api_key=key, http_client=httpx.Client(proxy=proxy))
-
-
-st.set_option('browser.serverAddress', 'localhost')
 st.set_page_config(
     page_title="AI Mentor",
     layout="centered",  # or "wide"
@@ -47,26 +33,23 @@ st.title("AI Mentor")
 st.markdown("### MCQ Generator")
 st.markdown("Загрузите учебные материалы и получите вопросы по теме.")
 
-open_ai_text_completion_model = CustomOpenAIModel(
-    key=api_key,
-    proxy=proxy_url,
+model = PerplexityModel(
+    api_key=api_key,
     parameters={
-        "model": "gpt-4-turbo-preview",
-        "temperature": 0.2,
-        "max_tokens": 1500,
+        "model": "pplx-70b-online",
     },
 )
 
 topic = st.chat_input("Enter Topic")
-ielts_agent = Agent(
+agent = Agent(
     role="Ielts expert",
     prompt_persona=f"Ваша задача — разработать 10 ВОПРОСОВ (MCQ) по {topic}, а также, дать на них ответы. Отвечать нужно только на русском языке"
 )
 
-ielts_task = Task(
+task = Task(
     name="get Ielts study",
-    model=open_ai_text_completion_model,
-    agent=ielts_agent,
+    model=model,
+    agent=agent,
     instructions="Give 10 MCQ Questions with answers",
 )
 
@@ -76,7 +59,7 @@ if topic:
         completion_message="pipeline completed",
         tasks=[
             # tasks are instance of Task class
-            ielts_task  # Task C
+            task  # Task C
         ],
     ).run()
 
